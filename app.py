64070f4b3dacc4cc255e396c8f54fa0a72d1cb71
Anirudh Gaur <65908151+Anirudh1308@users.noreply.github.com>
2023-04-20 04:19:10 -0400

    Flask connecting backend to frontend by Anirudh

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app= Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return "Welcome to this page"
-
-@app.route('/members')
-def members():
-    return "Welcome to this page peeps"
+    return render_template('index.html')
 
 
 if __name__== '__main__':
