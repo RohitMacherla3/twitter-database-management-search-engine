b02a679108f2d3c292c82bc8ec6fc04179699ade
Anirudh Gaur <65908151+Anirudh1308@users.noreply.github.com>
2023-04-20 05:10:14 -0400

    Flask adding hashtags and username by Anirudh

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app= Flask(__name__)
 
 @app.route('/')
 def welcome():
     return render_template('index.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        input_text = request.form['input-field']
+        if input_text.startswith('@'):
+            return redirect(url_for('username', username=input_text))
+        elif input_text.startswith('#'):
+            return redirect(url_for('hashtag', hashtag=input_text))
+        else:
+            # do something else if not username or hashtag
+            pass
+    return render_template('index.html')
+
+@app.route('/username/<username>')
+def username(username):
+    return render_template('username.html', username=username)
+
+@app.route('/hashtag/<hashtag>')
+def hashtag(hashtag):
+    return render_template('hashtag.html', hashtag=hashtag)
+
 
 
 if __name__== '__main__':
